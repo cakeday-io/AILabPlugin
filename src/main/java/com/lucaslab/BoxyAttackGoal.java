967d763a31ebd6f72581fda906eb76f6dd736c2d
Matt Paulin <matt@ministryofproduct.com>
2021-12-29 06:25:41 -0800

    Added an AI goal for giant
    Created a couple AI goals to test out what that can do.

---
+++ src/main/java/com/lucaslab/BoxyAttackGoal.java
@@ -0,0 +1,46 @@
+package com.lucaslab;
+
+import com.destroystokyo.paper.entity.ai.Goal;
+import com.destroystokyo.paper.entity.ai.GoalKey;
+import com.destroystokyo.paper.entity.ai.GoalType;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
+import org.bukkit.plugin.Plugin;
+
+import java.util.EnumSet;
+
+public class BoxyAttackGoal<T extends Mob> implements Goal {
+
+    private final Plugin plugin;
+    private final Mob mob;
+    private final LivingEntity target;
+
+    public BoxyAttackGoal(Plugin plugin, Mob mob, LivingEntity target) {
+        this.plugin =plugin;
+        this.mob=mob;
+        this.target=target;
+    }
+    @Override
+    public boolean shouldActivate() {
+        return true;
+    }
+
+    @Override
+    public GoalKey getKey() {
+        return GoalKey.of(Mob.class, new NamespacedKey(plugin, "BoxyGoal"));
+    }
+
+    @Override
+    public EnumSet<GoalType> getTypes() {
+        return EnumSet.of(GoalType.TARGET, GoalType.MOVE);
+    }
+
+    @Override
+    public void tick() {
+//        Pathfinder.PathResult pathResult = mob.getPathfinder().findPath(target);
+//        mob.getPathfinder().
+//         mob.setTarget(target);
+        mob.getPathfinder().moveTo(target);
+    }
+}
--- src/main/java/com/lucaslab/LucasLabPlugin.java
+++ src/main/java/com/lucaslab/LucasLabPlugin.java
@@ -1,9 +1,14 @@
 package com.lucaslab;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Giant;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -33,13 +38,24 @@ public class LucasLabPlugin extends JavaPlugin {
 
         LOG.info("Command Called: " + label);
         if (sender instanceof Player) {
-            Player me = (Player) sender;
+            Player thisPlayer = (Player) sender;
 
 
-            Location spot = me.getWorld().getSpawnLocation().clone();
+            Location spot = thisPlayer.getWorld().getSpawnLocation().clone();
+            Location spawnSpot = spot.clone();
+            spawnSpot.setX(spawnSpot.getX() + 20);
+            Location goSpot = spot.clone();
+            spawnSpot.setX(spawnSpot.getX() + 100);
+
             logSpot(spot);
 
+
             if (label.equalsIgnoreCase("lucaslab:test")) {
+                LOG.info("Creating Giant");
+
+                World world = spawnSpot.getWorld();
+                Mob waveMember = (Mob) world.spawnEntity(spawnSpot, EntityType.GIANT);
+                Bukkit.getMobGoals().addGoal(waveMember, 1, new BoxyMoveGoal<Giant>(this, waveMember, goSpot));
                 LOG.info("Lucas Lab Test Complete");
             }
         }
